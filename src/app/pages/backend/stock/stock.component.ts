6c4e170ce63a0a18a678836cec4667a7e445f705
Opongpon <o_pongpon@hotmail.com>
2022-02-21 15:03:44 +0700

    update CRUD

--- src/app/pages/backend/stock/stock.component.ts
+++ src/app/pages/backend/stock/stock.component.ts
@@ -11,12 +11,55 @@ export class StockComponent implements OnInit {
   //สร้างตัวแปรรับค่า api
   dataProduct: any = []
 
+  //ตัวแปรกำหนดค่าบนฟอร์มเพิ่มสินค้า
+  dataProductAdd = {
+    "productName":"",
+    "price":"",
+    "quantity":"",
+    "picture":"",
+    "categoryName":""
+  }
+
+  //เก็บรายการสินค้าตามไอดี
+  dataProductById = {
+    "productName":"",
+    "price":"",
+    "quantity":"",
+    "picture":"",
+    "categoryName":""
+  }
+
+  //เก็บข้อมูลสำหรับแก้ไข
+  dataProductEdit = {
+    "productId" : "",
+    "productName":"",
+    "price":"",
+    "quantity":"",
+    "picture":"",
+    "categoryName":""
+  }
+
   constructor(public api : ProductService) { }
 
   ngOnInit(): void {
     this.api.getProducts().subscribe((data: {}) => {
       this.dataProduct = data
     })
+  }
+  //ฟังชั่นเพิ่มรายการสินค้า
+  submitAddProduct(){
+    this.api.createProduct(this.dataProductAdd).subscribe((data: {}) => {
+      alert("Save Product Complete")
+    })
+  }
+  //ฟังชั่นแก้ไขสินค้า
+  submitEditProduct(){
+    
+  }
+
+  //ฟังชั่นสำหรับการลบข้อมูล
+  deleteProduct(){
+    alert("ลบข้อมูล")
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,19 +9,40 @@ import { ProductModel } from '../models/product.model';
 export class ProductService {
 
   //apiURL
-  apiURL = "https://localhost:5001/api/Product"
+  apiURL = "https://localhost:5001/api/"
 
   constructor(private http:HttpClient) {}
 
   //HttpHeader
   httpOptions = {
     headers : new HttpHeaders({
-      'Content-Type': 'application/Json'
+      'Content-Type': 'application/Json',
+      'Accept': 'application/Json'
     })
   }
 
-  //อ่านข้อมูล Product
+  //อ่านข้อมูลทั้งหมดของ Product (methodGet)
   getProducts() : Observable<ProductModel>{
-    return this.http.get<ProductModel>(this.apiURL)
+    return this.http.get<ProductModel>(this.apiURL + 'Product')
+  }
+
+  //อ่านข้อมูล ProductbyID (methodGet)
+  getProduct(id: string) : Observable<ProductModel>{
+    return this.http.get<ProductModel>(this.apiURL + 'Product/' + id)
+  }
+
+  //เพิ่มสินค้าใหม่ (methodPost)
+  createProduct(product: any) : Observable<ProductModel>{
+    return this.http.post<ProductModel>(this.apiURL + 'Product',JSON.stringify(product),this.httpOptions)
+  }
+
+  //แก้ไขข้อมูลสินค้า (methodPut)
+  updateProduct(id: string, product: any) : Observable<ProductModel>{
+    return this.http.put<ProductModel>(this.apiURL+ 'Product/' + id,JSON.stringify(product),this.httpOptions)
+  }
+
+  //ลบรายการสินค้า (methodDelete)
+  deleteProduct(id: string){
+    return this.http.delete<ProductModel>(this.apiURL + 'Product/' + id,this.httpOptions)
   }
 }
